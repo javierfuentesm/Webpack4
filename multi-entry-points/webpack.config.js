3696ae5030064342ad1c8dfc38b5f5bb4d5fa0d7
Rjfm <javier.fuentesm@hotmail.com>
2020-02-18 15:43:49 -0600

    Multi entries

---
+++ multi-entry-points/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports={
+    entry:{
+       home: path.resolve(__dirname,'src/js/index.js'),
+       precios: path.resolve(__dirname,'src/js/precio.js'),
+       contacto: path.resolve(__dirname,'src/js/contacto.js'),
+
+    },
+    mode: 'development',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: 'js/[name].js'
+    }
+}
