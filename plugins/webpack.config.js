d44e77dba16952e7f4099e19a362adf705b4afa9
Javier <javier.fuentesm@hotmail.com>
2020-02-26 17:40:42 -0600

    Plugins

---
+++ plugins/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, "src/js/index.js")
+  },
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCSSExtractPlugin.loader }, "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: "css/[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      title: "Plugins"
+    })
+  ]
+};
