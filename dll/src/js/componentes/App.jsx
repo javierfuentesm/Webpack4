40ece1a5a705d5e61b64cd3e4d79d4dd0e11c8dc
Javier <javier.fuentesm@hotmail.com>
2020-02-27 12:19:27 -0600

    DLL

---
+++ dll/src/js/componentes/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../css/sass.scss";
+import "../../css/less.less";
+import "../../css/stylus.styl";
+const App = () => (
+  <div>
+    <p className="sass">esto es sass</p>
+    <p className="less">esto es less</p>
+    <p className="stylus">estos es stylus</p>
+    <p className="post-css">estos es postcss</p>
+
+    <h1>Que Linda aplicacion en React.js</h1>
+  </div>
+);
+export default App;
---
+++ dll/src/js/contacto.js
@@ -0,0 +1,6 @@
+import "../css/estilos.css";
+import React from "react";
+import App from './componentes/App.jsx';
+import { render } from "react-dom";
+
+render(<App />, document.getElementById("container"));
