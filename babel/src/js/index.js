3b854f60c6987870eb619ab7b4bdb3a9c172484b
Javier <javier.fuentesm@hotmail.com>
2020-02-26 21:24:39 -0600

    Async con babel

--- babel/src/js/index.js
+++ babel/src/js/index.js
@@ -1,9 +1,13 @@
 import "../css/index.css";
-import text from "./text";
-text();
-if (module.hot) {
-  module.hot.accept("./text.js", function() {
-      console.log("He recargado en caliente")
-    text();
+import search from "./search";
+import render from "./render";
+
+const id = prompt("Quien es ese pokemon");
+search(id)
+  .then(data => {
+    console.log(data)
+    render(data);
+  })
+  .catch(() => {
+    console.log("No hubo pokemon");
   });
-}
--- babel/src/js/text.js
+++ babel/src/js/text.js
@@ -1,4 +1,4 @@
-function text() {
+const text =()=> {
     document.body.innerHTML="<h1>Si ves este texto por 199 segundos pasan cosas</h1>"
 }
 
